f054401a0c3e029e73ffac74e56ce41fd63b8146
dustypaulson <dustyplsn@gmail.com>
2017-04-07 12:50:53 -0500

    Adding NSG and UDR PS files

---
+++ ase-ilb-blueprint/UDR.ps1
@@ -0,0 +1,17 @@
+﻿#Update all variables for the resource group, location, UDRName, ExistingVnetName, and RouteName
+
+Login-AzureRmAccount
+$ResourceGroup = "Mayotest"
+$location = "CentralUS"
+$UDRName = "TestUDR"
+$ExistingVnetName = "VNet1"
+$routeName = "RouteToInternet"
+
+$route = New-AzureRmRouteConfig -Name $routeName -AddressPrefix 0.0.0.0/0 -NextHopType Internet
+$routeTable = New-AzureRmRouteTable -ResourceGroupName $ResourceGroup -Location $location -Name $UDRName -Route $route
+$vnet = Get-AzureRmVirtualNetwork -ResourceGroupName $ResourceGroup -Name $ExistingVnetName
+Set-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $Vnet.Subnets[0].Name -AddressPrefix $vnet.Subnets[0].AddressPrefix -RouteTable $routeTable
+Set-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $Vnet.Subnets[1].Name -AddressPrefix $vnet.Subnets[1].AddressPrefix -RouteTable $routeTable
+Set-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $Vnet.Subnets[2].Name -AddressPrefix $vnet.Subnets[2].AddressPrefix -RouteTable $routeTable
+Set-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $Vnet.Subnets[3].Name -AddressPrefix $vnet.Subnets[3].AddressPrefix -RouteTable $routeTable
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
