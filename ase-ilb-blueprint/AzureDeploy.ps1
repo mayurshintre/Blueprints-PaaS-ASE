246d92ef41ffcaee2eb66a31a935b3beec87ebd4
CosmicCubicle <admin@cosmiccubicle.com>
2017-05-12 09:45:25 -0500

    Testing Update

--- ase-ilb-blueprint/AzureDeploy.ps1
+++ ase-ilb-blueprint/AzureDeploy.ps1
@@ -10,7 +10,7 @@
     ##Azure Region to Deploy all resources including the Resource Group
     $Region = "West US"
     ##Name of the Resource Group to deploy
-    $RgName = "BluePrint"
+    $RgName = "BluePrint1"
     ##Name to give the Deployment that will be ran
     $DeploymentName = $RgName +"-nist800ase"
     ##Location of the main azuredeploy.json template
